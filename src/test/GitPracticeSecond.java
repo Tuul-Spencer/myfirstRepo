9f2f3bd56816f66032c9996b02c6e6712a1176f1
Tuul-Spencer <125428731+Tuul-Spencer@users.noreply.github.com>
2023-02-22 00:11:56 -0500

    Update GitPracticeSecond.java
    This is practice for pull again

--- src/test/GitPracticeSecond.java
+++ src/test/GitPracticeSecond.java
@@ -6,6 +6,7 @@ public class GitPracticeSecond {
 		
 		System.out.println("This is for push practice again and again");
 		System.out.println("This is for pull practice");
+		System.out.println("This is practice for pull again");
 
 	}
 
